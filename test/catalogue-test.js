d3f6118969e7d63e0ea028c3d6802c7110aee2a3
yuxuan wang <2879296442@qq.com>
2020-10-27 22:31:01 +0000

    Forgot boundary tests

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -49,5 +49,15 @@ describe("Catalogue", () => {
         expect(result.productIds).to.have.lengthOf(2);
         expect(result.productIds).to.have.members(["B123", "B124"]);
       });
+      it("should include products just on their reorder level", function () {
+        cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
+        const result = cat.checkReorders();
+        expect(result.productIds).to.have.members(["B125"]);
+      });
+      it("should handle an empty catalogue", function () {
+        cat = new Catalogue("Test catalogue");
+        const result = cat.checkReorders();
+        expect(result.productIds).to.be.empty;
+      });
     });
 });
